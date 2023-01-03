2bc9363409fb7dce98964ba4b8d296d154254e3c
mariareeves <mariapaula.zimmers@gmail.com>
2023-01-03 12:33:44 -0500

    redid the board

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// console.log('Testing')
+
+
+/*----- constants -----*/
+
+
+
+
+
+/*----- app's state (variables) -----*/
+// variables to compare the match later
+// let cardOne, cardTwo;
+
+
+
+
+/*----- cached element references -----*/
+const cardsEl = document.querySelector('.cards')
+
+
+
+
+/*----- event listeners -----*/
+
+
+
+
+
+
+
+/*----- functions -----*/
