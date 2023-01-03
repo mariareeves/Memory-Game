f5c02aed12170ddcece9d060a1e35acead208392
mariareeves <mariapaula.zimmers@gmail.com>
2023-01-03 15:50:58 -0500

    one cards is flipping

--- js/main.js
+++ js/main.js
@@ -1,20 +1,25 @@
 // console.log('Testing')
 
+// 1- add event listerns to the cards
+// 2- make the flip once clicked
+// 3-not possible to click in the same pic twice in a row
+// 4- switch player every two cards clickes 
+// 5- flip last two cards once hide button was clicked
+// 6- count pair matches
+// 7- winning conditionals
+// 8- restart button unflips all cards at once and restart players count
+// 9- display winning message
 
-/*----- constants -----*/
 
-
-
-
-
-/*----- app's state (variables) -----*/
 // variables to compare the match later
-// let cardOne, cardTwo;
+let cardOne, cardTwo;
+let isCardFlipped = false;
+let isBoardLocked = false;
 
 
 
 
-/*----- cached element references -----*/
+// dom elements
 const cardsEl = document.querySelector('.cards')
 
 
@@ -22,10 +27,14 @@ const cardsEl = document.querySelector('.cards')
 
 /*----- event listeners -----*/
 
+cardsEl.addEventListener('click', cardFlip)
 
 
 
 
 
+// /*----- functions -----*/
+function cardFlip() {
+    cardsEl.classList.toggle('cardflip')
+}
 
-/*----- functions -----*/
